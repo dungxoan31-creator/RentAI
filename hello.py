3f308e69fdf044a0816e399b790509c9a9a7fcfa
dungxoan31-creator <dungxoan31@gmail.com>
2025-10-22 22:13:25 +0700

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel(r"C:\Users\dungx\Downloads\batdongsan_no_address.xlsx", engine="openpyxl")
-print(data.head())  # chỉ in 5 dòng đầu để tránh tràn màn hình
-
-import ski
